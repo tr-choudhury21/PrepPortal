9ce8015dd4de5ad7f65236d3809d424ec3d7a237
Titu Ray choudhury <tituraychoudhury@gmail.com>
2024-10-28 17:46:31 +0530

    resolve cors error

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,11 @@ const PORT = process.env.PORT || 5000
 const app = express();
 
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"]  //Add your allowed headers here if needed.
+}));
 app.use(express.json());
 
 
