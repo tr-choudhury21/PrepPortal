057656c8e36d31c568980ea702840c1fe302fc4f
Titu Ray Choudhury <tituraychoudhury@gmail.com>
2025-04-02 18:34:58 +0530

    add social media links in user model

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -3,38 +3,41 @@ const validator = require("validator");
 const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
-    fullName: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        validate: [validator.isEmail, "Please provide a valid Email."]
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    avatar: {  // New: Store profile picture URL
-        type: String,
-        default: "https://via.placeholder.com/150"
-    },
-    bio: {  // New: Short bio
-        type: String,
-        default: "Hey there! I'm using this platform."
-    },
-    socialLinks: {  // New: Object to store multiple social media links
-        github: { type: String, default: "" },
-        linkedin: { type: String, default: "" },
-        twitter: { type: String, default: "" }
-    }
+  fullName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    validate: [validator.isEmail, "Please provide a valid Email."],
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    // New: Store profile picture URL
+    type: String,
+    default: "https://via.placeholder.com/150",
+  },
+  bio: {
+    // New: Short bio
+    type: String,
+    default: "Hey there! I'm using this platform.",
+  },
+  socialLinks: {
+    // New: Object to store multiple social media links
+    github: { type: String, default: "" },
+    linkedin: { type: String, default: "" },
+    twitter: { type: String, default: "" },
+  },
 });
 
 userSchema.methods.generateJsonWebToken = function () {
-    return jwt.sign({ id: this._id }, process.env.JWT_SECRET_KEY, {
-        expiresIn: process.env.JWT_EXPIRES,
-    });
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: process.env.JWT_EXPIRES,
+  });
 };
 
-module.exports = mongoose.model("Users", userSchema);
+module.exports = mongoose.model("User", userSchema);
