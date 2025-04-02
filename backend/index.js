69f4f765d67272ba7a070784b1245c7a56c9aad0
Titu Ray Choudhury <tituraychoudhury@gmail.com>
2025-04-02 18:35:56 +0530

    add new routes

--- backend/index.js
+++ backend/index.js
@@ -4,33 +4,36 @@ const cookieParser = require("cookie-parser");
 const dbConnection = require("./database/dbConnection");
 const UserRoutes = require("./routes/userRoutes");
 const DocumentRoutes = require("./routes/docRoutes");
-const appointmentRoutes = require("./routes/appointmentRoutes")
+const appointmentRoutes = require("./routes/appointmentRoutes");
+const QnaRoutes = require("./routes/qnaRoutes");
+const BlogRoutes = require("./routes/blogRoutes");
 
 require("dotenv").config();
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 app.use(cookieParser());
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CLIENT_URL,
     credentials: true,
-    allowedHeaders: ["Content-Type", "Authorization"]  //Add your allowed headers here if needed.
-}));
+    allowedHeaders: ["Content-Type", "Authorization"], //Add your allowed headers here if needed.
+  })
+);
 app.use(express.json());
-
 
 //routes
 app.use("/api/v1/auth", UserRoutes);
 app.use("/api/v1/docs", DocumentRoutes);
+app.use("/api/v1/qna", QnaRoutes);
+app.use("/api/v1/blog", BlogRoutes);
 app.use("/appointment", appointmentRoutes);
-
 
 //database connection
 dbConnection();
 
-
-app.listen(PORT, ()=>{
-    console.log(`Server started on Port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server started on Port ${PORT}`);
 });
